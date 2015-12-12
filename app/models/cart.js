6b52cd05145b291f8ccd2eb573cd76a0a8686f7c
Rodolfo Valguarnera <rod7val@gmail.com>
2015-12-12 02:02:55 -0300

    simple vista de Cart con jade template

--- app/models/cart.js
+++ app/models/cart.js
@@ -8,4 +8,24 @@ if (typeof localStorage === "undefined" || localStorage === null) {
 
 var Cart = shop(config.cartname, true);
 
+// Cart.addItem({
+// 	id:10,
+// 	name:'Superman Poster',
+// 	desc:'10x5 - superman logo',
+// 	price:12.5,
+// 	qty:2,
+// 	image:'/img/shop/superman.png'
+// });
+
+// Cart.addItem({
+// 	id:9,
+// 	name:'Sony TV',
+// 	desc:'21" - TV Sony',
+// 	price:7.5,
+// 	qty:5,
+// 	image:'/img/shop/tv.png'
+// });
+
+// Cart.save();
+
 module.exports = Cart;
